5439193d7c23d3c01dabbfa97bb2894762e1afe3
Ed Doris <ed.doris@gmail.com>
2020-06-06 21:14:16 -0500

    reconfigured database and fixed flask app

--- Templates/app.py
+++ Templates/app.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-# from mongoengine.connection import connect
 import os
-import jinja2
+
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory
+
+# from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config['MONGO_DBNAME'] = 'Project2'
-mongo = PyMongo(app, uri="mongodb://localhost:27017/Project2")
+# app.config['MONGO_DBNAME'] = 'Global_Energy_Analysis'
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/Global_Energy_Analysis")
 
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+db = client.Global_Energy_Analysis
+# collection = db.produce
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
     # Find one record of data from the mongo database
-    Project2 = mongo.db.Project2.find({})
+    # Project2 = list(db.Project2.find())
 
     # Return the homepage
     return render_template('index.html')
@@ -26,52 +34,52 @@ def home():
 @app.route("/TEC.html")
 def TEC2():
     # Find one record of data from the mongo database
-    TEC2 = mongo.db.TEC2.find({})
+    TEC = list(db.TEC.find())
 
 
     # Return template and data
-    return render_template('TEC.html', TEC2 = TEC2)
+    return render_template('TEC.html', TEC = TEC)
 
 # Route to render TEP.html template using data from Mongo
 @app.route("/TEP.html")
 def TEP():
     # Find one record of data from the mongo database
-    TotalEnergyProduction = mongo.db.TotalEnergyProduction.find({})
-
+    TEP = list(db.TEP.find())
+    print(TEP)
 
 
     # Return template and data
-    return render_template('TEP.html', TotalEnergyProduction = TotalEnergyProduction)
+    return render_template('TEP.html', TEP = TEP)
 
 # # Route to render EBOT.html template using data from Mongo
 @app.route("/EBOT.html")
 def EBOT():
     # Find one record of data from the mongo database
-    EnergyBalanceOfTrade = mongo.db.EnergyBalanceOfTrade.find({})
+    EBOT = list(db.EBOT.find())
 
 
     # Return template and data
-    return render_template('EBOT.html', EnergyBalanceOfTrade = EnergyBalanceOfTrade)
+    return render_template('EBOT.html', EBOT = EBOT)
 
 # # Route to render EIG.html template using data from Mongo
 @app.route("/EIG.html")
 def EIG():
     # Find one record of data from the mongo database
-    EnergyIntensityGDP = mongo.db.EnergyIntensityGDP.find({})
+    EIG = list(db.EIG.find())
 
 
     # Return template and data
-    return render_template('EIG.html', EnergyIntensityGDP = EnergyIntensityGDP)
+    return render_template('EIG.html', EIG = EIG)
 
 # # Route to render CC.html template using data from Mongo
-@app.route("/CC.html")
-def CC():
+# @app.route("/CC.html")
+# def CC():
     # Find one record of data from the mongo database
-    coordinates = mongo.db.coordinates.find({})
+    # coordinates = mongo.db.coordinates.find({})
 
 
     # Return template and data
-    return render_template('CC.html', coordinates = coordinates)
+    # return render_template('CC.html', coordinates = coordinates)
 
 if __name__ == "__main__":
     app.run(debug=True)
